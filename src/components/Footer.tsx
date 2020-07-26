2f1734d98ba002800a212806ba0f627a0234b053
Matt Williamson <mathewbwilliamson@gmail.com>
2020-07-26 01:14:39 -0400

    Updated CSS

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,9 +5,8 @@ interface FooterProps {}
 export const Footer: React.FC<FooterProps> = () => {
   return (
     <div className='footer flex w-full flex flex-col mt-8'>
-      <div className='mn-bg-black h-3 w-full'></div>
       <div className='mn-bg-yellow h-3 w-full'></div>
-      <div className='mn-bg-light-red h-10 w-full'></div>
+      <div className='mn-bg-light-red h-24 w-full'></div>
     </div>
   );
 };
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,14 +8,9 @@ interface HeaderProps {
 export const Header: React.FC<HeaderProps> = ({ isAdmin = false }) => {
   return (
     <div className='header flex w-full flex flex-col items-center mb-8 bg-white'>
-      <div className='mn-bg-yellow h-2 w-full mb-4'></div>
-      <div className='flex w-full flex flex-row items-center pb-4'>
+      <div className='flex w-full flex flex-row items-center p-10 mn-bg-black text-white'>
         <div className='ml-12 w-full'>This is the header</div>
         {isAdmin && <CreateProductButton />}
-      </div>
-      <div className='h-4 w-full'>
-        <div className='mn-bg-black h-2 w-full'></div>
-        <div className='mn-bg-light-red h-2 w-full'></div>
       </div>
     </div>
   );
