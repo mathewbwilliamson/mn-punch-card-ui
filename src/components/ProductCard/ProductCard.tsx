2c5c196b3742f8d040c714b43ae279298142e3e6
Matt Williamson <mathewbwilliamson@gmail.com>
2020-07-27 00:06:02 -0400

    Added edit and delete buttons

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Product } from '../../types/productTypes';
 import { calculateRewardCardPrice } from '../../utils/calculateRewardCardPrice';
+import { Button } from '../atomics/Button';
 
 interface ProductCardProps {
   productData?: Product;
@@ -20,6 +21,10 @@ export const ProductCard: React.FC<ProductCardProps> = ({ productData }) => {
       </div>
       <img src={productData.imageUrl} alt={`${productData.title}`} />
       <div className='text-gray-600 text-lg'>{productData.title}</div>
+      <div className='flex flex-row justify-end w-full absolute bottom-0 right-0 my-4 mt-6'>
+        <Button buttonClassNames='mr-4 px-4 py-1' text='Delete' />
+        <Button buttonClassNames='mr-4 px-4 py-1' text='Edit' />
+      </div>
     </div>
   );
 };
