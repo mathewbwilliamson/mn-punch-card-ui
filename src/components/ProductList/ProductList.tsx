9ed4e7985fe17f2760557dfedbff062a7be0ee30
Matt Williamson <mathewbwilliamson@gmail.com>
2020-08-01 14:12:55 -0400

    Removed console log

--- src/components/ProductList/ProductList.tsx
+++ src/components/ProductList/ProductList.tsx
@@ -17,11 +17,6 @@ export const ProductList: React.FC<ProductListProps> = ({
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  console.log(
-    '\x1b[44m%s \x1b[0m',
-    '[matt] state.',
-    state.ProductListStore.sortedProductList
-  );
   return (
     <div className='product-list__container grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-2 mx-12 gap-8'>
       {state.ProductListStore.sortedProductList.map((product) => (
