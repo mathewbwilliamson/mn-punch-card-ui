beaf899bcdb853fe67ef775b738a9d62dda2f2c0
Matt Williamson <mathewbwilliamson@gmail.com>
2020-08-01 15:44:09 -0400

    Add footer, header images

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ interface FooterProps {}
 export const Footer: React.FC<FooterProps> = () => {
   return (
     <div className='footer flex w-full flex flex-col mt-8'>
-      <div className='mn-bg-yellow h-3 w-full'></div>
+      <div className='mn-bg-yellow h-6 w-full'></div>
       <div className='mn-bg-light-red h-24 w-full'></div>
     </div>
   );
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { CreateProductButton } from './CreateProduct/CreateProductButton';
+import logo from '../assets/images/Mathnasium_logo.png';
 
 interface HeaderProps {
   isAdmin?: boolean;
@@ -9,9 +10,8 @@ export const Header: React.FC<HeaderProps> = ({ isAdmin = false }) => {
   return (
     <div className='header flex w-full flex flex-col items-center mb-8 bg-white'>
       <div className='flex w-full flex flex-row items-center py-10 pl-12 mn-bg-black text-white'>
-        <h2 className='text-3xl w-full font-serif'>
-          Mathnasium of New Tampa Reward Cabinet
-        </h2>
+        <img src={logo} alt='logo' className='h-16 mb-2 mr-4' />
+        <h2 className='text-3xl w-full font-serif'>New Tampa Reward Cabinet</h2>
         {isAdmin && <CreateProductButton />}
       </div>
     </div>
